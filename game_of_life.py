0034900695945f703177e3bc08aaa1ffd30316f7
Julian Groen <julian.groen@wgu.edu>
2024-07-27 00:04:40 -0600

    add assets file and updated to render sprites

--- game_of_life.py
+++ game_of_life.py
@@ -3,18 +3,26 @@ import random
 
 import pyxel
 
+WINDOW_WIDTH = 256
+WINDOW_HEIGHT = 256
+CELL_SIZE = 8
+STATE_WIDTH = int(WINDOW_WIDTH / CELL_SIZE)
+STATE_HEIGHT = int(WINDOW_HEIGHT / CELL_SIZE)
+BPM = 120
+
 
 class App:
     def __init__(self):
-        pyxel.init(64, 64, title="Conway's Game of Life", fps=60)
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title="Conway's Game of Life", fps=60)
+        pyxel.load("./assets.pyxres")
         # self.cell_state = [
         #     [False for _ in range(pyxel.width)] for _ in range(pyxel.height)
         # ]
         self.cell_state = [
-            [random.choice([True, False]) for _ in range(pyxel.width)]
-            for _ in range(pyxel.height)
+            [random.choice([True, False]) for _ in range(STATE_WIDTH)]
+            for _ in range(STATE_HEIGHT)
         ]
-        self.running = True
+        self.running = False
         pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
@@ -33,7 +41,7 @@ class App:
         total = 0
 
         for nx, ny in neighbors:
-            if 0 <= x + nx < pyxel.width and 0 <= y + ny < pyxel.height:
+            if 0 <= x + nx < STATE_WIDTH and 0 <= y + ny < STATE_HEIGHT:
                 if self.cell_state[y + ny][x + nx]:
                     total += 1
 
@@ -45,7 +53,6 @@ class App:
         for y_index, y_value in enumerate(self.cell_state):
             for x_index, x_value in enumerate(y_value):
                 alive_neighbors = self.neighborhood_watch(x_index, y_index)
-                print(alive_neighbors) if alive_neighbors > 1 else None
 
                 if x_value:
                     if alive_neighbors < 2 or alive_neighbors > 3:
@@ -65,23 +72,32 @@ class App:
             self.running = not self.running
         if pyxel.btnp(pyxel.KEY_R):
             self.cell_state = [
-                [random.choice([True, False]) for _ in range(pyxel.width)]
-                for _ in range(pyxel.height)
+                [random.choice([True, False]) for _ in range(STATE_WIDTH)]
+                for _ in range(STATE_HEIGHT)
+            ]
+        if pyxel.btnp(pyxel.KEY_C):
+            self.cell_state = [
+                [False for _ in range(STATE_WIDTH)] for _ in range(STATE_HEIGHT)
             ]
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            self.cell_state[pyxel.mouse_y][pyxel.mouse_x] = not self.cell_state[
-                pyxel.mouse_y
-            ][pyxel.mouse_x]
+            mx = int(pyxel.mouse_x / CELL_SIZE)
+            my = int(pyxel.mouse_y / CELL_SIZE)
+            self.cell_state[my][mx] = not self.cell_state[my][mx]
         if self.running:
-            if pyxel.frame_count % 16 == 0:
+            if pyxel.frame_count % 30 == 0:
                 self.automata()
 
     def draw(self):
         pyxel.cls(0)
         for y_index, y_value in enumerate(self.cell_state):
             for x_index, x_value in enumerate(y_value):
+                x_loc = x_index * CELL_SIZE
+                y_loc = y_index * CELL_SIZE
+                pyxel.dither(0.5)
+                pyxel.blt(x_loc, y_loc, 0, 16, 0, CELL_SIZE, CELL_SIZE)
+                pyxel.dither(1)
                 if x_value:
-                    pyxel.pset(x_index, y_index, 6)
+                    pyxel.blt(x_loc, y_loc, 0, 0, 0, CELL_SIZE, CELL_SIZE, 0)
 
 
 App()
